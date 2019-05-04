b352fca3e6fad1a5b6c5675f08f52e6649e51bf9
Tony <lin-tony@users.noreply.github.com>
2019-05-04 18:36:16 +0800

    加入均值哈希去重

--- README.md
+++ README.md
@@ -15,12 +15,37 @@ Windows SDK版本：10.0.17134.0
 
 平台：Visual Studio 2017 (v141)
 
+# 去重
+## 均值哈希 average_hash
+average_hash是perceptual hash算法的一种。
+
+过程：
+1. 图像缩放—将图像缩放到N*N大小
+2. 灰度化—对N*N大小的图像进行灰度化
+3. 计算均值—计算这N*N大小图片中所有像素的均值
+4. 得到N*N图像的ahash—N*N的像素值中大于均值的则用1表示，小于的用0表示，这样就得到一个N*N位二进制码作为该图像的ahash值。
+5. 计算两幅图像ahash值的汉明距离，距离越小，表明两幅图像越相似；距离越大，表明两幅图像距离越大。
+
+程序中我用了8*8，阈值为5，如果觉得精度不高可以增加为16*16阈值为3等等
+
+
+__优点__
+简单，计算快
+
+__缺点__
+敏感性较高，图片稍微变化对结果影响极大
+
+头文件dirent.h使用了JackSoja写的兼容windows的文件 https://github.com/JackSoja/dirent
+
+
+
 
 # To do list
 - <del>正则将url里分辨率匹成1920*1080</del>
 - 增加下载以前的图片的功能
 - 随机更换
 - 去重
+
 
 # else
 关于url参数 "http://cn.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1"
---
+++ average_hash.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <Windows.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgproc/types_c.h"
+
+using namespace std;
+using namespace cv;
+
+#define N 8
+
+//使用感知哈希算法进行图片去重
+//MatStruct：存储对应名称和要比较的内容（N*N的灰度图）
+typedef struct MatStruct {
+	string name;
+	unsigned char buf[N*N];
+	MatStruct() {
+		name = "";
+		memset(buf, 0, N*N);
+	}
+	MatStruct(const struct MatStruct &ms) {
+		name = ms.name;
+		memset(buf, 0, N*N);
+		memcpy(buf, ms.buf, N*N);
+	}
+	//重载比较函数
+	bool operator<(const struct MatStruct &ms)const {
+		return name < ms.name;
+	}
+}MatStruct;
+
+//获取该目录下所有文件存入images
+void GetDirImages(const string path, std::vector<MatStruct> &images) {
+	cout << "\nGet dir------------" << endl;
+	std::vector<MatStruct> tmpimages;
+	DIR *dir;
+	struct dirent *ptr;
+	//打开文件目录且遍历
+	dir = opendir(path.c_str());
+	while ((ptr = readdir(dir)) != NULL) {
+		if ((strcmp(ptr->d_name, ".") == 0) || (strcmp(ptr->d_name, "..") == 0)) {
+			continue;
+		}
+		string fullname = path + "/" + ptr->d_name;
+		Mat img = imread(fullname);
+		if (img.empty()) {
+			continue;
+		}
+		MatStruct ms;
+		ms.name = ptr->d_name;
+		Mat gray, res;
+		//缩放成NxN大小灰度图
+		resize(img, res, Size(N, N));
+		//转化成灰度图
+		cvtColor(res, gray, CV_RGB2GRAY);
+		//获取灰度平均值
+		double mn = mean(gray)[0];
+		//比较像素灰度，获取图像指纹
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				ms.buf[i * N + j] = (gray.at<unsigned char>(i, j) > mn) ? 1 : 0;
+			}
+		}
+		cout << "get " << fullname << endl;
+		tmpimages.push_back(ms);
+	}
+	closedir(dir);
+	std::sort(tmpimages.begin(), tmpimages.end());
+	tmpimages.swap(images);
+}
+
+//删除图像
+void DeleteImage(const string path, std::vector<std::vector<MatStruct>::iterator> &delImages, std::vector<MatStruct> &images) {
+	//必须从后往前删，从前面开始的话会导致delImages[i+1]以及之后存储的迭代器失效
+	for (int i = delImages.size() - 1; i >= 0; i--) {
+		cout << "Remove " << delImages[i]->name << endl;
+		string fullpath = path + "/" + delImages[i]->name;
+		unlink(fullpath.c_str());
+		images.erase(delImages[i]);
+	}
+}
+
+//比较两个图片的相似度
+bool IsTheSame(std::vector<MatStruct>::iterator x, std::vector<MatStruct>::iterator y) {
+	int diff = 0;
+	if (x != y) {
+		//计算相似度(这里的计算方法也称hammingDistance汉明距离)，不同的地方不超过5（阈值可以自己确定），则为相似的图片
+		for (int i = 0; i < N*N; i++) {
+			if (x->buf[i] != y->buf[i]) {
+				diff++;
+			}
+		}
+		return diff <= 5;
+	}
+	return false;
+}
+
+void CompareImage(const string path, std::vector<MatStruct> &images) {
+	cout << "\nStart compare-----------" << endl;
+	std::vector<MatStruct>::iterator it1 = images.begin();
+	while (it1 != images.end()) {
+		std::vector<MatStruct>::iterator it2 = it1;
+		//存储需要删除的迭代对象
+		std::vector<std::vector<MatStruct>::iterator> delImages;
+		cout << it1->name << endl;
+		while (it2 != images.end()) {
+			if (IsTheSame(it2, it1)) {
+				delImages.push_back(it2);
+			}
+			it2++;
+		}
+		if (delImages.size() > 0) {
+			DeleteImage(path, delImages, images);
+		}
+		it1++;
+	}
+}
+
+void AverageHash(const string path) {
+	std::vector<MatStruct> images;
+	GetDirImages(path, images);
+	CompareImage(path, images);
+}
+
+int main(int argc, char* argv[]) {
+	if (argc <= 1) {
+		cout << "Usage: " << argv[0] << " picturePath" << endl;
+		return -1;
+	}
+
+	struct stat st;
+	//必须有该路径且是目录
+	if (stat(argv[1], &st) != 0 || (st.st_mode & S_IFDIR) != S_IFDIR) {
+		cout << "Error! " << argv[1] << " is not dir" << endl;
+		return -1;
+	}
+	AverageHash(argv[1]);
+	return 0;
+}
